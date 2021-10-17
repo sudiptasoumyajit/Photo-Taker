72f09bb3c842aa4d23cd71f8265e627649f1f629
sudiptasoumyajit <84839625+sudiptasoumyajit@users.noreply.github.com>
2021-10-17 14:50:30 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -14,5 +14,19 @@ function takeSnapshot(){
 console.log('ml5 version', ml5.version);
 classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/cJFTQdXTE/model.json',modelLoaded);
 function modelLoaded(){
-    console.log('Your Camera Is Working, We Take The Image And Seeing For The Model.')
+    console.log('Your Camera Is Working, We Take The Image And Seeing For The Model.');
+}
+function check() {
+    img = document.getElementById('capturedImage');
+    classifier.classify(img,goResult);
+}
+function goResult(error,results) {
+    if (error) {
+        console.error(Error);
+    }
+    else {
+        console.log(results);
+        document.getElementById("resultObjectName").innerHTML = results[0].label;
+        document.getElementById("resultObjectAccuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
 }
